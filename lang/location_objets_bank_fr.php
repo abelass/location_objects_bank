d1bd313d3ce8f101433aeeaea9255ebf60caecdc
abelass <rainer@websimple.be>
2018-06-06 16:11:02 +0200

    La fabrique

---
+++ lang/location_objets_bank_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'location_objets_bank_titre' => 'Location d’objets - paiements',
+);
---
+++ location_objets_bank_pipelines.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Utilisations de pipelines par Location d’objets - paiements
+ *
+ * @plugin     Location d’objets - paiements
+ * @copyright  2018
+ * @author     Rainer Müller
+ * @licence    GNU/GPL v3
+ * @package    SPIP\Location_objets_bank\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
